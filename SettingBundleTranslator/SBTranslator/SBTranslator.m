78e94e55d7ff5e906246c4b259500f7f7b8f395b
Wang Jinghan <phantom.there@gmail.com>
2015-01-12 03:18:20 +0800

    Demo file updated

--- SettingBundleTranslator/SBTranslator/SBTranslator.m
+++ SettingBundleTranslator/SBTranslator/SBTranslator.m
@@ -27,6 +27,7 @@
 - (void) updateSettingItems {
     NSString *plistPath = [[NSBundle mainBundle] pathForResource:@"Settings.bundle/Root" ofType:@"plist"];
     NSMutableDictionary *settings = [[NSMutableDictionary alloc] initWithContentsOfFile:plistPath];
+    NSLog(@"%@", settings);
     self.settingGroups = [self getItemsFromDictionaryArray:[settings objectForKey:@"PreferenceSpecifiers"]];
 }
 
